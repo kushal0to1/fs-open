8c87fa02196e6c0f5049eefc98c5927cadeb1344
kushal0to1 <kushalkalyanpm@gmail.com>
2024-04-15 17:11:39 +0530

    exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,19 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
+
+const Statistics = (props) => {
+   return (
+    <div>
+       <h1>Statistics</h1>
+       <p>Good {props.good}</p>
+       <p>Neutral {props.neutral}</p>
+       <p>Bad {props.bad}</p>
+       <p>All {props.sum}</p>
+       <p>Average {props.average}</p>
+       <p>Positive {props.percentage} %</p>
+    </div>
+   ) 
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -12,14 +27,15 @@ const App = () => {
       <button onClick={()=> setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good - bad)/(good + neutral + bad)}</p>
-      <p>Positive {(good/(good + neutral + bad))*100} %</p>
+      <Statistics 
+        good={good}
+        neutral={neutral} 
+        bad={bad} 
+        sum={(good + neutral + bad)} 
+        average={(good - bad) / (good + neutral + bad)}
+        percentage={(good / (good + neutral + bad)) * 100}
+         />
+      
     </div>
   )
 }
