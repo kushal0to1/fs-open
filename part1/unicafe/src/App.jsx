75676c4143539145e1ce34c01894db45ef2ee05c
kushal0to1 <kushalkalyanpm@gmail.com>
2024-04-15 17:46:43 +0530

    exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,38 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <p>{props.text} {props.value}</p>
+  )
+}
+
 const Statistics = (props) => {
   if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
     return (
       <div>
         <h1>Statistics</h1>
-        <p>No feedback given</p>
+        <StatisticLine text="No feedback given"  />
       </div>
     )
   }
   return (
     <div>
       <h1>Statistics</h1>
-      <p>Good {props.good}</p>
-      <p>Neutral {props.neutral}</p>
-      <p>Bad {props.bad}</p>
-      <p>All {props.sum}</p>
-      <p>Average {props.average}</p>
-      <p>Positive {props.percentage} %</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.sum} />
+      <StatisticLine text="average" value={props.average} />
+      <StatisticLine text="positive" value={`${props.percentage} %`} />
     </div>
   )
 }
@@ -33,9 +47,9 @@ const App = () => {
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Button text="Good" handleClick={() => setGood(good + 1)} />
+      <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="Bad" handleClick={() => setBad(bad + 1)}  />
 
       <Statistics
         good={good}
