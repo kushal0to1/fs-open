78dab70e588948636a47e1101d8404e4ceb03c71
kushal0to1 <kushalkalyanpm@gmail.com>
2024-04-18 11:02:32 +0530

    exercise 2.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,44 +1,27 @@
 /* eslint-disable react/prop-types */
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+import React from "react"
 
-
-
-
+const Course = ({course}) => {
+  console.log(course)
   const Header = (props) => {
+    console.log(props.course)
     return (
-      <h1>{props.course.name}</h1>
-      
+      <h1>{props.course}</h1>
+
     )
   }
 
   const Content = (props) => {
-    
+
     return (
-<div>
-  
-       { props.parts.map((p, index) => (
-          <Part key={index} partName={p.name} exercises={p.exercises} />
+      <div>
+
+        {props.parts.map((p) => (
+          <Part key={p.id} partName={p.name} exercises={p.exercises} />
         ))}
-      
-</div>
-      )
+
+      </div>
+    )
   }
 
   const Total = (props) => {
@@ -52,18 +35,43 @@ const App = () => {
   }
 
   const Part = (props) => {
-  return(
-    <p>{props.partName} {props.exercises}</p>
-  )
+    return (
+      <p>{props.partName} {props.exercises}</p>
+    )
   }
-
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts}  />
-      <Total parts={course.parts} />
-    </div>
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    {/* <Total parts={course.parts} /> */}
+  </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  console.log('app component')
+  return( <Course course={course} />)
 }
 
 export default App
--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -1,5 +1,7 @@
-import ReactDOM from 'react-dom/client'
-
+import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
