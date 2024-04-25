ae103efd061966224933085ed77c2d1ca360f303
kushal0to1 <kushalkalyanpm@gmail.com>
2024-04-25 10:03:47 +0530

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const Filter = ({search, handleSearch}) => {
   return (
     <div>Filter shown with: <input type="text" value={search} onChange={handleSearch} placeholder="Search..." /></div>
@@ -47,16 +49,17 @@ const Persons = ({ filteredPersons }) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [number, setNumber] = useState('');
   const [search, setSearch] = useState('');
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+  })}, [])
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
